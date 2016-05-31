547233dc1ac3f6e53c91e4c77532c99a997c8367
Ted Karanikolas <ted@hrg-nyc.com>
2016-05-30 22:41:36 -0500

    first commit

---
+++ scripts/javascripts.js
@@ -0,0 +1,52 @@
+//you will need to create an application that records employees along with their salary. We also want to add the salaries up so we know how much we’re spending each month.
+//First have an input form that collects: The Employee's First and Last name The Employee's ID Number The Employee's Job Title The Employee's Salary (Yearly)
+//The form should have a submit button that clears out the form and your logic should store that information.
+// Then, that information should be appended to the DOM so the user of the application can see the information they just entered.
+//Finally, your logic should calculate all of the employee salaries and report back what the monthly cost of salaries is.
+//console.log("wassup?");
+var newEmployeeList = [];
+
+var createNewEmployee = function(){
+  var firstName = document.getElementById('firstName').value;
+  var lastName = document.getElementById('lastName').value;
+  var companyID = document.getElementById('companyID').value;
+  var jobTitle = document.getElementById('jobTitle').value;
+  var empSalary = document.getElementById('empSalary').value;
+  document.getElementById("myForm").reset();
+  //var empName = document.getElementById('firstName ' + 'lastName');
+
+  var newEmployee = {
+    'firstName' : firstName,
+    'lastName' : lastName,
+    'companyID' : companyID,
+    'jobTitle' : jobTitle,
+    'empSalary': empSalary
+  };
+
+
+newEmployeeList.push(newEmployee);
+console.log(newEmployeeList.length);
+console.log(newEmployeeList);
+listEmp();
+};
+
+
+//Displays employees in the DOM
+var listEmp = function(){
+  document.getElementById("output").innerHTML = "";
+  for( var i = 0; i< newEmployeeList.length; i++ ){
+    var empDisplay ="First Name: " + newEmployeeList[i].firstName + ";  Last Name: " + newEmployeeList[i].lastName + ";  CompanyID: " + newEmployeeList[i].companyID + ";  Job Title: " + newEmployeeList[i].jobTitle + "; Employee Salary: " + parseInt(newEmployeeList[i].empSalary);
+    document.getElementById("output").innerHTML += '<p>' + empDisplay + '</p>';
+  }
+};
+
+//console.log(newEmployeeList[i][4]);
+
+
+
+//listEmp();
+//createNewEmployee();
+
+
+
+//document.getElementById('newEmployeeList').innerHTML = "newEmployeeList";
